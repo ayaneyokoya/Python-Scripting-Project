f4b3c3bb6ee0974891a25d84ff61e953faa61f64
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-10 13:55:19 -0400

    implemented a method to get filename & strip

--- get_game_data.py
+++ get_game_data.py
@@ -20,13 +20,31 @@ def find_all_game_paths(source):
     
     return game_paths
 
+def get_name_from_paths(paths, to_strip):
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip, '')
+        new_names.append(new_dir_name)
+    
+    return new_dir_name
+
+def create_dir(path):
+    if not os.path.exisst(path):
+        os.mkdir(path)
+
 def main(source, target):
     cwd = os.getcwd()
     # will join the path based on operating system
     source_path = os.path.join(cwd, source) 
     target_path = os.path.join(cwd, target) 
     
+    game_paths = find_all_game_paths(source_path)
     
+    # will give just the directory name but removes "game"
+    new_game_dirs = get_name_from_paths(game_paths, "game")
+    
+    create_dir(target_path)
 
 # checks that file was ran directly 
 # won't execute anything if a function or class were imported from this file
