c5329fe3c0e4f76708673a8c3dd051994e6a6f32
Ayane Yokoya <ayaneyokoya@gmail.com>
2023-07-10 13:41:43 -0400

    added main

--- get_game_data.py
+++ get_game_data.py
@@ -2,4 +2,14 @@ import os
 import json # to work w/ json files
 import shutil # will allow copying & overriding operations
 from subprocess import PIPE, run # will allow running almost any terminal command
-import sys # access to command line arguments
+import sys # access to command line arguments  
+
+# checks that file was ran directly 
+# won't execute anything if a function or class were imported from this file
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 3:
+        raise Exception("You must pass a source and target directory only.")
+    
+    # strip off name of python file & get two arguments to be stored into separate variables
+    source, target = args[1:]
